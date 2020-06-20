54eab173dbd13fedf6f475a970fdbb34ea476f4a
Vítor Norberto <vithor.norberto@gmail.com>
2020-06-20 10:11:39 -0300

    Adiciona Enum para status da vaga e corrige data para Padrao iso-8601

--- src/main/java/com/reactive/smartparking/api/domain/Vaga.java
+++ src/main/java/com/reactive/smartparking/api/domain/Vaga.java
@@ -6,26 +6,19 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.validation.constraints.NotNull;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
+import javax.validation.constraints.NotBlank;
+import java.time.OffsetDateTime;
 
 @Document @NoArgsConstructor @AllArgsConstructor @Data
 public class Vaga {
+
     @Id
     private String id;
 
-    @NotNull(message = "Nome deve ser preenchido")
+    @NotBlank(message = "Nome deve ser preenchido")
     private String nome;
 
-    @NotNull(message = "Estado da vaga deve ser preenchido")
-    private boolean status;
+    private StatusVaga status;
 
-    private String data = new Date()
-            .toInstant()
-            .atZone(ZoneId.of("Brazil/East"))
-            .format(DateTimeFormatter
-                    .ofPattern("dd/MM/yyyy HH:mm:ss"));
-    // TODO: 06/06/2020 refatorar essa data como string, deveria estar usando um tipo espefico para data/hora com zoneId
+    private OffsetDateTime data = OffsetDateTime.now();
 }
